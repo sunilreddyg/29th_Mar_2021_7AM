71afb29f0f60a21e8c6592a2c81c68918dea4aff
sunilreddyG1 <sunny.user2@rediffmail.com>
2021-06-18 08:57:25 +0530

    Variable Class1

---
+++ Project_189/src/mq/java/variable/Java_Concatenation.java
@@ -0,0 +1,18 @@
+package mq.java.variable;
+
+public class Java_Concatenation {
+
+	public static void main(String[] args) 
+	{
+		
+		
+		int a=100;
+		int b=200;
+		
+		int x=a+b;			//Here + symbol conducting addition
+		System.out.println("total value is -> "+x);		//Here + club two value
+		System.out.println("total value is-> "+(a+b));  
+
+	}
+
+}
